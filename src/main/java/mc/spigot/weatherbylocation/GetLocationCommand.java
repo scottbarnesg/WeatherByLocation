7ee38bedaf37441465ed27236e375c9233942fa4
Scott Barnes <scottgbarnes@gwu.edu>
2023-05-31 21:22:44 -0500

    Release 0.2.9 (#29)
    * Refactor plugin startup and messages
    
    * Update gitignore
    
    ---------
    
    Co-authored-by: Scott Barnes <sgbarnes@protonmail.com>

--- src/main/java/mc/spigot/weatherbylocation/GetLocationCommand.java
+++ src/main/java/mc/spigot/weatherbylocation/GetLocationCommand.java
@@ -17,7 +17,13 @@ public class GetLocationCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         ServerLocator.LocationData locationData = plugin.getLocationData();
-        String message = String.format("Location used for weather data is (%.3f, %.3f)", locationData.latitude, locationData.longitude);
+        String message;
+        if (locationData.countryCode != null && locationData.region != null && locationData.city != null) {
+            message = String.format("Location used for weather data %s, %s, %s at (%.3f, %.3f)", locationData.city, locationData.region, locationData.countryCode, locationData.latitude, locationData.longitude);
+        }
+        else {
+            message = String.format("Location used for weather data is (%.3f, %.3f)", locationData.latitude, locationData.longitude);
+        }
         if (sender instanceof Player) {
             Player player = (Player) sender;
             player.sendMessage(ChatColor.AQUA + message);
