ea6cf303eb464c30ab1464097c1b064275d16db4
Scott Barnes <scottgbarnes@gwu.edu>
2023-06-09 22:04:43 -0500

    Support changes to update interval without server restart (#33)
    * Working implementation of dynamic update interval
    
    * Update SetUpdateInterval command to match dynamic updates
    
    * Improve logs
    
    ---------
    
    Co-authored-by: Scott Barnes <sgbarnes@protonmail.com>

--- src/main/java/mc/spigot/weatherbylocation/SetUpdateInterval.java
+++ src/main/java/mc/spigot/weatherbylocation/SetUpdateInterval.java
@@ -18,9 +18,7 @@ public class SetUpdateInterval implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        // Validate inputs
-        // check for wrong number of arguments
+        // Validate input
         if (args.length != 1) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
@@ -29,8 +27,7 @@ public class SetUpdateInterval implements CommandExecutor {
             getLogger().info("Invalid input provided. Expected format is /set-weather-update-interval XX");
             return false;
         }
-
-        // check for non-numeric inputs
+        // Validate input type
         String minutes = args[0];
         int interval ;
         try {
@@ -39,34 +36,27 @@ public class SetUpdateInterval implements CommandExecutor {
         catch (NumberFormatException e) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                player.sendMessage(ChatColor.DARK_RED + "Bad input provided. Input is to be positive number of 1 or more and the expected format is /set-weather-update-interval XX");
+                player.sendMessage(ChatColor.DARK_RED + "Invalid input provided. Update interval must be a positive integer.");
             }
-            getLogger().info("Bad input provided. Input is to be positive number of 1 or more and the expected format is /set-weather-update-interval XX");
+            getLogger().info("Invalid input provided. Update interval must be a positive integer.");
             return false;
         }
-
-        // check for input being outside of accepted limits
-        if(interval < 1){
+        if (interval < 1) {
             if (sender instanceof Player) {
                 Player player = (Player) sender;
-                player.sendMessage(ChatColor.DARK_RED + "Input value too low. Input is to be positive number of 1 or more and the expected format is /set-weather-update-interval XX");
+                player.sendMessage(ChatColor.DARK_RED + "Invalid input provided. Update interval must be a positive integer.");
             }
-            getLogger().info("Input value too low. Input is to be positive number of 1 or more and the expected format is /set-weather-update-interval XX");
+            getLogger().info("Invalid input provided. Update interval must be a positive integer.");
             return false;
         }
-
         // Write values to config file
         plugin.getConfig().set("minutes-between-updates", interval);
         plugin.saveConfig();
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            player.sendMessage(ChatColor.AQUA + String.format("Set update interval to (%d) minutes", interval));
-            player.sendMessage(ChatColor.DARK_RED + String.format("New changes to be reflected upon restart of server or reload of plugin"));
+            player.sendMessage(ChatColor.AQUA + String.format("Set update interval to %d minutes. This will take effect after the next weather update.", interval));
         }
-        getLogger().info(String.format("Set update interval to (%d) minutes", interval));
-        getLogger().info(String.format("New changes to be reflected upon restart of server or reload of plugin"));
-
+        getLogger().info(String.format("Set update interval to %d minutes. This will take effect after the next weather update.", interval));
         return true;
     }
-
 }
